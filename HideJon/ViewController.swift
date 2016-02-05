ed2b0517da8f551259ed25c4aaa9dc6958fda1e5
Adam Roberts <Adam@promac.example.com>
2016-02-05 14:51:44 -0600

    added files for Hide Jon

--- HideJon/ViewController.swift
+++ HideJon/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var JonHead: UIImageView!
+    @IBOutlet weak var MichaelHead: UIImageView!
+    @IBOutlet weak var JonButton: UIButton!
+    @IBOutlet weak var MichaelButton: UIButton!
+    @IBOutlet weak var ShowBothButton: UIButton!
+    @IBOutlet weak var lilJon: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +28,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func HideJon(sender: AnyObject) {
+        JonHead.hidden = true
+        MichaelHead.hidden = false
+    }
+    
+    @IBAction func MichaelHide(sender: AnyObject) {
+        MichaelHead.hidden = true
+        JonHead.hidden = false
+    }
 
+    @IBAction func ShowBoth(sender: AnyObject) {
+        JonHead.hidden = false
+        MichaelHead.hidden = false
+        lilJon.hidden = false
+    }
+    
+    
+    @IBAction func GoAwayLilJon(sender: AnyObject) {
+        lilJon.hidden = true
+    }
 }
 
